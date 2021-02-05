db0798670e08957ab0adb5d326af5545f0817f40
Pavel Ulianov <paveluv@mail.ru>
2021-02-05 13:04:21 +0800

    2.3 Add routing.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import SingInScreen from "../sign-in-screen/sign-in-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import MovieScreen from "../movie-screen/movie-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import PlayerScreen from "../player-screen/player-screen";
+import ErrorScreen from "../error-screen/error-screen";
+
+const CountCard = {
+  MY_LIST: 4,
+  SAME: 5
+};
 
 const App = (props) => {
   const {cardsCount, movieTitle, movieGenre, movieYear, isLogin} = props;
 
   return (
-    <MainScreen
-      cardsCount={cardsCount}
-      movieTitle={movieTitle}
-      movieGenre={movieGenre}
-      movieYear={movieYear}
-      isLogin={isLogin}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen
+            cardsCount={cardsCount}
+            movieTitle={movieTitle}
+            movieGenre={movieGenre}
+            movieYear={movieYear}
+            isLogin={isLogin}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SingInScreen isLogin={isLogin} />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListScreen
+            isLogin={isLogin}
+            cardsCount={CountCard.MY_LIST}
+          />
+        </Route>
+        <Route exact path="/films/:id">
+          <MovieScreen
+            isLogin={isLogin}
+            cardsCount={CountCard.SAME}
+          />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewScreen isLogin={isLogin} />
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerScreen />
+        </Route>
+        <Route>
+          <ErrorScreen isLogin={isLogin} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 

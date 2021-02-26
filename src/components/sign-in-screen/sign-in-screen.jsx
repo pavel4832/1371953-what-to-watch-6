7a44d942870c0d241816538ca43c3a42498b59f4
Pavel Ulianov <paveluv@mail.ru>
2021-02-26 14:25:03 +0800

    6.2.1 Refactor sign-in-screen.jsx.

--- src/components/sign-in-screen/sign-in-screen.jsx
+++ src/components/sign-in-screen/sign-in-screen.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import Header from "../header/header";
-import Footer from "../footer/footer";
-import PropTypes from "prop-types";
+import React, {useRef} from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import {login} from '../../store/api-actions';
 
 const SingInScreen = (props) => {
-  const {isLogin} = props;
+  const {isLogin, onSubmit} = props;
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const history = useHistory();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
 
   return (
     <div className="user-page">
@@ -17,19 +32,43 @@ const SingInScreen = (props) => {
       />
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form
+          action="#"
+          className="sign-in__form"
+          onSubmit={handleSubmit}
+        >
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email"/>
+              <input
+                ref={loginRef}
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email"
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password"/>
+              <input
+                ref={passwordRef}
+                className="sign-in__input"
+                type="password"
+                placeholder="Password"
+                name="user-password"
+                id="user-password"
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
           <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
+            <button
+              onClick={() => history.push(`/`)}
+              className="sign-in__btn"
+              type="submit"
+            >
+              Sign in
+            </button>
           </div>
         </form>
       </div>
@@ -41,11 +80,18 @@ const SingInScreen = (props) => {
 
 SingInScreen.propTypes = {
   isLogin: PropTypes.bool.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   isLogin: state.isLogin,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  }
+});
+
 export {SingInScreen};
-export default connect(mapStateToProps, null)(SingInScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(SingInScreen);
